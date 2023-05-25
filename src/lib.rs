87a4db8a22ce78f1e86af29970039cd3603fc289
José Molina <jose.molina@parity.io>
2023-05-25 11:35:48 +0200

    add pallet logic

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+//! # Blacklist Pallet
+//!
+//! - [`Config`]
+//!
+//! ## Overview
+//!
+//! Allow some configurable origin: [`Config::BlacklistingOrigin`] to blacklist some accounts.
+//! Blacklisted accounts are not allowed to perform
+
+#![cfg_attr(not(feature = "std"), no_std)]
+pub use pallet::*;
+
+// #[cfg(feature = "runtime-benchmarks")]
+// mod benchmarking;
+#[cfg(test)]
+mod mock;
+#[cfg(test)]
+mod tests;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use frame_system::WeightInfo;
+
+    use super::*;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        /// Required origin for whitelisting an account.
+        type BlacklistingOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+
+        /// The weight information for this pallet.
+        type WeightInfo: WeightInfo;
+    }
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub (super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        AccountBlacklisted { account: T::AccountId },
+        BlacklistedAccountRemoved { account: T::AccountId },
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The account was not blacklisted.
+        AccountIsNotBlacklisted,
+        /// The account was already blacklisted; No-Op.
+        AccountAlreadyBlacklisted,
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn blacklist)]
+    pub type BlacklistedAccount<T: Config> =
+    StorageMap<_, Twox64Concat, T::AccountId, (), OptionQuery>;
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(0)]
+        pub fn blacklist_account(origin: OriginFor<T>, account: T::AccountId) -> DispatchResult {
+            T::BlacklistingOrigin::ensure_origin(origin)?;
+
+            ensure!(
+				!BlacklistedAccount::<T>::contains_key(&account),
+				Error::<T>::AccountAlreadyBlacklisted,
+			);
+
+            BlacklistedAccount::<T>::insert(&account, ());
+
+            Self::deposit_event(Event::<T>::AccountBlacklisted { account });
+
+            Ok(())
+        }
+
+        #[pallet::call_index(1)]
+        #[pallet::weight(0)]
+        pub fn remove_blacklisted_account(
+            origin: OriginFor<T>,
+            account: T::AccountId,
+        ) -> DispatchResult {
+            T::BlacklistingOrigin::ensure_origin(origin)?;
+
+            BlacklistedAccount::<T>::take(&account).ok_or(Error::<T>::AccountIsNotBlacklisted)?;
+
+            Self::deposit_event(Event::<T>::BlacklistedAccountRemoved { account });
+
+            Ok(())
+        }
+    }
+
+    impl <T: Config> Pallet<T> {
+        pub fn is_account_blacklisted(account: &T::AccountId) -> bool {
+            BlacklistedAccount::<T>::take(&account).is_some()
+        }
+    }
+
+    impl<T: Config> EnsureOrigin<T::AccountId> for Pallet<T> {
+        type Success = ();
+
+        fn try_origin(o: T::AccountId) -> Result<Self::Success, T::AccountId> {
+            match Self::is_account_blacklisted(&o) {
+                false => Ok(()),
+                true => Err(o)
+            }
+        }
+
+        #[cfg(feature = "runtime-benchmarks")]
+        fn try_successful_origin() -> Result<T::AccountId, ()> {
+            Err(())
+        }
+    }
+}
---
+++ src/tests.rs
@@ -0,0 +1,84 @@
+use crate::{mock::*, Error, Event};
+use frame_support::{assert_noop, assert_ok};
+use sp_runtime::traits::BadOrigin;
+
+#[test]
+fn it_successfully_inserts_blacklisted_accounts() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(1);
+        // At the beginning there is no account blacklisted
+        assert_eq!(BlacklistModule::blacklist(42), None);
+        // we insert it now
+        assert_ok!(BlacklistModule::blacklist_account(RuntimeOrigin::root(), 42));
+        // check the account is there
+        assert_eq!(BlacklistModule::blacklist(42), Some(()));
+        assert_eq!(BlacklistModule::is_account_blacklisted(&42), true);
+        System::assert_last_event(Event::AccountBlacklisted { account: 42 }.into());
+    });
+}
+
+#[test]
+fn only_root_can_add_to_blacklist() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(1);
+        // if Root is not performing the operation it should fail
+        assert_noop!(
+			BlacklistModule::blacklist_account(RuntimeOrigin::signed(1), 42),
+			BadOrigin
+		);
+    });
+}
+
+#[test]
+fn error_if_inserting_again() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(1);
+        assert_ok!(BlacklistModule::blacklist_account(RuntimeOrigin::root(), 42));
+
+        // try and insert again the same value
+        assert_noop!(
+			BlacklistModule::blacklist_account(RuntimeOrigin::root(), 42),
+			Error::<Test>::AccountAlreadyBlacklisted
+		);
+    });
+}
+
+#[test]
+fn it_successfully_removes_blacklisted_accounts() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(1);
+        // insert the blacklisted account
+        assert_ok!(BlacklistModule::blacklist_account(RuntimeOrigin::root(), 42));
+        // remove the account
+        assert_eq!(BlacklistModule::remove_blacklisted_account(RuntimeOrigin::root(), 42), Ok(()));
+        System::assert_last_event(Event::BlacklistedAccountRemoved { account: 42 }.into());
+        // check it is no longer there
+        assert_eq!(BlacklistModule::blacklist(42), None);
+        assert_eq!(BlacklistModule::is_account_blacklisted(&42), false);
+    });
+}
+
+#[test]
+fn only_root_can_remove_from_blacklist() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(1);
+        assert_ok!(BlacklistModule::blacklist_account(RuntimeOrigin::root(), 42));
+        // if Root is not performing the operation it should fail
+        assert_noop!(
+            BlacklistModule::remove_blacklisted_account(RuntimeOrigin::signed(1), 42),
+            BadOrigin
+        );
+    });
+}
+
+#[test]
+fn error_if_account_is_not_blacklisted() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(1);
+        // cannot remove the account from the blacklist if it is not there
+        assert_noop!(
+            BlacklistModule::remove_blacklisted_account(RuntimeOrigin::root(), 42),
+            Error::<Test>::AccountIsNotBlacklisted
+        );
+    });
+}
