bce390f6593578194e4491c8e646a7bbf5ee2376
José Molina <jose.molina@parity.io>
2023-09-16 17:38:35 +0200

    feat: upgrade to polkadot-v1.0.0

--- src/mock.rs
+++ src/mock.rs
@@ -3,39 +3,33 @@ use frame_support::traits::{ConstU16, ConstU64};
 use frame_system::EnsureRoot;
 use sp_core::H256;
 use sp_runtime::{
-    testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
+    BuildStorage,
 };
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-    pub enum Test where
-        Block = Block,
-        NodeBlock = Block,
-        UncheckedExtrinsic = UncheckedExtrinsic,
-    {
+    pub struct Test {
         System: frame_system,
         BlacklistModule: pallet_blacklist,
     }
 );
 
 impl frame_system::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
     type BaseCallFilter = frame_support::traits::Everything;
     type BlockWeights = ();
     type BlockLength = ();
     type RuntimeOrigin = RuntimeOrigin;
     type RuntimeCall = RuntimeCall;
-    type Index = u64;
-    type BlockNumber = u64;
+    type Nonce = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
     type AccountId = u64;
     type Lookup = IdentityLookup<Self::AccountId>;
-    type Header = Header;
-    type RuntimeEvent = RuntimeEvent;
+    type Block = Block;
     type BlockHashCount = ConstU64<250>;
     type DbWeight = ();
     type Version = ();
@@ -57,8 +51,8 @@ impl pallet_blacklist::Config for Test {
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    frame_system::GenesisConfig::default()
-        .build_storage::<Test>()
+    frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
         .unwrap()
         .into()
 }
